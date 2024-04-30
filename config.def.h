40257ab25c97fcc6743cba7027cb0c30aa2afdab
George Armstrong <georgea117@gmail.com>
2024-04-30 22:45:53 +1200

    apply `focusonclick` patch

--- config.def.h
+++ config.def.h
@@ -5,6 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0;
 static char font[]                  = "monospace:size=10";
 static const char *fonts[]          = { font };
 static char col_fg[]          = "#ffffff";
